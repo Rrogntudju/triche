ad895ed807de0e6dcb55600c6bb215b3239778eb
Rrogntudju <rrogntudju@example.com>
2022-10-15 12:25:39 -0400

    wip

--- crates/triche/src/main.rs
+++ crates/triche/src/main.rs
@@ -4,104 +4,109 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn valide_position(arg: &str) -> Result<(char, usize), String> {
-   if arg.len() == 2 {
-      match valide_lettre(arg) {
-         Ok(c) => {
-               let n = arg.chars().nth(1).unwrap();
-               if "12345".contains(n) {
-                  Ok((c, n.to_string().parse::<usize>().unwrap() - 1))
-               } else {
-                  Err("la position n'est pas 1-5".to_owned())
-               }
-         }
-         Err(e) => Err(e),
-      }
-   } else {
-      Err("la longueur != 2".to_owned())
-   }
+    if arg.len() == 2 {
+        match valide_lettre(arg) {
+            Ok(c) => {
+                let n = arg.chars().nth(1).unwrap();
+                if "12345".contains(n) {
+                    Ok((c, n.to_string().parse::<usize>().unwrap() - 1))
+                } else {
+                    Err("la position n'est pas 1-5".to_owned())
+                }
+            }
+            Err(e) => Err(e),
+        }
+    } else {
+        Err("la longueur != 2".to_owned())
+    }
 }
 
 fn valide_lettre(arg: &str) -> Result<char, String> {
-   let c = arg.chars().nth(0).unwrap();
-   if c.is_ascii_alphabetic() {
-      Ok(c.to_ascii_lowercase())
-   } else {
-      Err("la lettre n'est pas a-z".to_owned())
-   }
-}
-
-fn créer_filtres(v: bool, j: bool, n: bool) -> Vec<Box<dyn FnMut(&[char; 5]) -> bool>> {
-   let mut filtres: Vec<Box<dyn FnMut(&[char; 5]) -> bool>> = Vec::new();
-   if n {
-      filtres.push(Box::new(|mot: &[char; 5]| { true }));
-   }
-   if j {
-      filtres.push(Box::new(|mot: &[char; 5]| { true }));
-   }
-   if v {
-      filtres.push(Box::new(|mot: &[char; 5]| { true }));
-   }
-   filtres
+    let c = arg.chars().nth(0).unwrap();
+    if c.is_ascii_alphabetic() {
+        Ok(c.to_ascii_lowercase())
+    } else {
+        Err("la lettre n'est pas a-z".to_owned())
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-   let matches = Command::new("triche")
-      .version("0.1.0")
-      .arg(
-         Arg::new("verte")
-               .help("position des lettres vertes. Ex: a1 r2 o3 s4 e5")
-               .short('v')
-               .long("verte")
-               .num_args(ValueRange::new(1..=5))
-               .action(ArgAction::Append)
-               .value_parser(valide_position),
-      )
-      .arg(
-         Arg::new("jaune")
-               .help("position des lettres jaunes.  Ex: e1 a3")
-               .short('j')
-               .long("jaune")
-               .num_args(ValueRange::new(1..=5))
-               .action(ArgAction::Append)
-               .value_parser(valide_position),
-      )
-      .arg(
-         Arg::new("noire")
-               .help("lettres noires.  Ex: r s")
-               .short('n')
-               .long("noire")
-               .num_args(ValueRange::new(1..=5))
-               .action(ArgAction::Append)
-               .value_parser(valide_lettre),
-      )
-      .get_matches();
+    let matches = Command::new("triche")
+        .version("0.1.0")
+        .arg(
+            Arg::new("verte")
+                .help("position des lettres vertes. Ex: a1 r2 o3 s4 e5")
+                .short('v')
+                .long("verte")
+                .num_args(ValueRange::new(1..=5))
+                .action(ArgAction::Append)
+                .value_parser(valide_position),
+        )
+        .arg(
+            Arg::new("jaune")
+                .help("position des lettres jaunes.  Ex: e1 a3")
+                .short('j')
+                .long("jaune")
+                .num_args(ValueRange::new(1..=5))
+                .action(ArgAction::Append)
+                .value_parser(valide_position),
+        )
+        .arg(
+            Arg::new("noire")
+                .help("lettres noires.  Ex: r s")
+                .short('n')
+                .long("noire")
+                .num_args(ValueRange::new(1..=5))
+                .action(ArgAction::Append)
+                .value_parser(valide_lettre),
+        )
+        .get_matches();
 
-   let vertes = matches.get_many("verte").unwrap_or_default().collect::<Vec<&(char, usize)>>();
-   let jaunes = matches.get_many("jaune").unwrap_or_default().collect::<Vec<&(char, usize)>>();
-   let noires = matches.get_many("noire").unwrap_or_default().collect::<Vec<&char>>();
-   let mut filtres = créer_filtres(!vertes.is_empty(), !jaunes.is_empty(), !noires.is_empty());
-   let mut filtre = match filtres.pop() {
-      Some(filtre) => filtre,
-      _ => return Err("Pas de filtre".into())
-   };
-   let fichier = File::open("english-words.txt")?;
-   let fichier = BufReader::new(fichier);
-   let mut mots: Vec<[char; 5]> = Vec::new();
+    let vertes = matches.get_many("verte").unwrap_or_default().collect::<Vec<&(char, usize)>>();
+    let jaunes = matches.get_many("jaune").unwrap_or_default().collect::<Vec<&(char, usize)>>();
+    let noires = matches.get_many("noire").unwrap_or_default().collect::<Vec<&char>>();
+    let mut filtres: Vec<Box<dyn FnMut(&[char; 5]) -> bool>> = Vec::new();
+    if !noires.is_empty() {
+        filtres.push(Box::new(|mot: &[char; 5]| true));
+    }
+    if !jaunes.is_empty() {
+        filtres.push(Box::new(|mot: &[char; 5]| true));
+    }
+    if !vertes.is_empty() {
+        let filtre = |mot: &[char; 5]| {
+            let trouvées = vertes.iter().fold(0, |mut acc, &v| {
+                if mot[v.1] == v.0 {
+                    acc += 1;
+                }
+                acc
+            });
+            trouvées == vertes.len()
+        };
+        filtres.push(Box::new(filtre));
+    }
 
-   for mot in fichier.lines() {
-      match mot {
-         Ok(mot) if mot.len() == 5 => {
-            let mot = mot.to_ascii_lowercase();
-            let mut m = [' '; 5];
-            mot.char_indices().for_each(|(i, c)| m[i] = c);
-            if filtre(&m) {
-               mots.push(m);
+    let mut filtre = match filtres.pop() {
+        Some(filtre) => filtre,
+        _ => return Err("Pas de filtre".into()),
+    };
+    let fichier = File::open("english-words.txt")?;
+    let fichier = BufReader::new(fichier);
+    let mut mots: Vec<[char; 5]> = Vec::new();
+
+    for mot in fichier.lines() {
+        match mot {
+            Ok(mot) if mot.len() == 5 => {
+                let mot = mot.to_ascii_lowercase();
+                let mut m = [' '; 5];
+                mot.char_indices().for_each(|(i, c)| m[i] = c);
+                if filtre(&m) {
+                    mots.push(m);
+                }
             }
-         }
-         Ok(_) => continue,
-         Err(e) => return Err(e.into()),
-      }
-   }
+            Ok(_) => continue,
+            Err(e) => return Err(e.into()),
+        }
+    }
 
-   Ok(())
+    Ok(())
 }
